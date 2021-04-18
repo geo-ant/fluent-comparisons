578365720fae7d6fee82ff6f66bbbe379c15e2b0
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-04-18 21:17:45 +0200

    fix TODO in documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-//! TODO Document this library
+//! This library provides a number of macros to make multicomparison expressions less annoying to
+//! write while still keeping the benefits of hand-written code, such as lazy evaluation and
+//! boolean short circuiting. Go to any of the macros to find out about their usage.
 
 pub use fluent_comparisons_macros::any_of;
 
