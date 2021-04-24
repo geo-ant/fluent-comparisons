5cef7964455c059306b056c035e1e6806fa9d8b7
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-04-24 21:24:27 +0200

    add approval tests for macro expansio of none_of and all_of

---
+++ macro_expansion_tests/none_of_expansion.expanded.rs
@@ -0,0 +1,17 @@
+use fluent_comparisons::none_of;
+struct Dummy {
+    pub length: usize,
+}
+pub fn something() {
+    let first = {
+        !(1 < 4) && !(2 < 4) && !(3 < 4)
+    };
+    let v = Dummy { length: 2 };
+    let second = {
+        !(v.length == v.length) && !(2_usize.pow(2) == v.length) && !(3 * 4 + 1 == v.length)
+    };
+    let square = |x| x * x;
+    let third = {
+        !(4 + 4 + 1 <= 8) && !(square(7 * 2) <= 8) && !(120_i32.pow(2) <= 8)
+    };
+}
