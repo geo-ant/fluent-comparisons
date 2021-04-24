148690c14910f50d843f4e40ab3fd8fd249dc25e
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-04-24 20:28:34 +0200

    add approval tests using the macrotest crate for any_of macro

---
+++ src/tests/approval.rs
@@ -0,0 +1,9 @@
+//! Approval tests for macro expansion using the [macrotest](https://crates.io/crates/macrotest) crate.
+
+#[test]
+/// test that the macro expansion is the same as a manually approved expansion test file
+fn any_of_macro_expansion_is_same_as_approved_expansion() {
+    // to update: delete the .expansion.rs file
+    // and replace macrotest::expand_without_refresh with macrotest::expand
+    macrotest::expand("macro_expansion_tests/any_of_expansion.rs")
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,4 +1,5 @@
 mod all_of;
 mod any_of;
+mod approval;
 mod none_of;
 mod theorems;
