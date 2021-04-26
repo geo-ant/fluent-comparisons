4009e91ae280f63a8932a97448f11ecc40c762d8
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-04-26 20:30:19 +0200

    finish tests for none_of

--- src/tests/none_of_with_map.rs
+++ src/tests/none_of_with_map.rs
@@ -1,6 +1,5 @@
 use crate::none_of;
 use rand::prelude::*;
-use std::cell::Cell;
 
 // helper function to double a value
 fn twice(val: usize) -> usize {
@@ -10,81 +9,44 @@ fn twice(val: usize) -> usize {
 #[test]
 fn none_of_comparisons_give_correct_result_for_operator_equal() {
     // test simple expressions
-    assert_eq!(none_of!({ 4 } == 2), true);
-    assert_eq!(none_of!({ 4 } == 4), false);
-    assert_eq!(none_of!({"four","two"} == "three"), true);
-    assert_eq!(none_of!({"four","two"} == "two"), false);
-    assert_eq!(none_of!({"two","two"} == "two"), false);
-    assert_eq!(none_of!({"two","four"} == "two"), false);
+    assert_eq!(none_of!({ 2 }.map(|x:i32|-x) == 2), true);
+    assert_eq!(none_of!({ 2 }.map(|x:i32|2*x) == 4), false);
+    assert_eq!(none_of!({3,2,1}.map(|x:i32|x-2)<0),false);
+    assert_eq!(none_of!({3,2,1}.satisfy(|x|x%4==0)),true);
 
     // more complex expressions
     let v = vec![1, 2, 3];
     let two = 2;
-    assert_eq!(none_of!( {6,2*v.len(),twice(3)} == twice(2)+two), false);
-    assert_eq!(none_of!( {6+2,2*v.len(),twice(3)} == 3+3), false);
-    assert_eq!(none_of!( {6*6,200*v.len(),twice(3)} == 4+2), false);
-    assert_eq!(none_of!( {6+1,2*v.len()+2,twice(3)/2} == 5+1), true);
+    assert_eq!(none_of!( {1,2*v.len()-1,twice(4)}.map(|x|x+1) == twice(2)+two), false);
+    assert_eq!(none_of!( {6+2,2*v.len(),twice(3)}.satisfy(|x|x==6)), false);
 
     // more complex expr
     assert_eq!(
-        none_of!( {2_i32.pow(2)+2,2*v.len(),4+2,666-660} == 6),
+        none_of!( {2_i32.pow(2)+2,2*v.len() as i32,4+2,666}.map(|x|x-660) == 6),
         false
     );
-    assert_eq!(none_of!( {4,4,4,1,-1,2,4} == 3), true);
-    assert_eq!(
-        none_of!( {6,6,6,6,6,2_i32.pow(2)+2,2*v.len(),1234} == 6),
-        false
-    );
-
-    todo!()
+    assert_eq!(none_of!( {4,4,4,1,3,2,4}.map(|x|2*x) == 3), true);
 }
 
 #[test]
 fn test_none_of_comparisons_for_other_operators() {
     let v = vec!["hello", "there", "this", "is", "a", "test"];
-
+    let twice = |x|x*2;
     // !=
-    assert_eq!(none_of!({6,2_usize.pow(2),v.len(),7}!=6), false);
-    assert_eq!(none_of!({6,2_usize.pow(2)+2,v.len(),7-1}!=6), true);
+    assert_eq!(none_of!({3,2_usize.pow(2),v.len(),7}.map(twice)!=6), false);
+    assert_eq!(none_of!({6,2_usize.pow(2)+2,v.len(),7-1}.satisfy(|x|x!=6)), true);
     // <=
-    assert_eq!(none_of!({7,8,9}<=6), true);
-    assert_eq!(none_of!({6,7,8,9}<=6), false);
+    assert_eq!(none_of!({7,8,9}.satisfy(|x|x<=6)), true);
+    assert_eq!(none_of!({7,7,8,9}.map(|x|x-1)<=6), false);
     // >=
-    assert_eq!(none_of!({7,8,9}>=6), false);
-    assert_eq!(none_of!({4,4,3,1}>=6), true);
+    assert_eq!(none_of!({6,6,6}.map(|x|2*x+3)>=6), false);
+    assert_eq!(none_of!({4,4,3,1}.satisfy(|x|x>=6)), true);
     // >
-    assert_eq!(none_of!({6,1,2,7,8,9}>6), false);
-    assert_eq!(none_of!({4,4,3,1,5}>6), true);
+    assert_eq!(none_of!({6,1,2,7,8,9}.satisfy(|x|x>6)), false);
+    assert_eq!(none_of!({4,4,3,1,5}.satisfy(|c|c>6)), true);
     // <
-    assert_eq!(none_of!({6,7,8,9}<6), true);
-    assert_eq!(none_of!({7,8,6,2,7}<6), false);
-    todo!()
-}
-
-#[test]
-fn expressions_are_short_circuited_and_evaluated_left_to_right() {
-    // use this as a variable that indicates mutable state
-    // and helps me count how often the twice function was invoked
-    let counter: Cell<i32> = Cell::new(0);
-    let twice = |i: i32| {
-        counter.set(counter.get() + 1);
-        2 * i
-    };
-
-    let eval = none_of!({1,2,3,4,5,twice(3)}==5);
-    assert!(!eval);
-    assert_eq!(counter.get(), 0);
-
-    let eval = none_of!({1,twice(3),twice(3),3}==6);
-    assert!(!eval);
-    assert_eq!(counter.get(), 1);
-    counter.set(0);
-
-    // this proves that the array way of doing things requires an eager evaluation
-    let eval = [1, twice(3), twice(3), 3].iter().all(|val| (val != &6));
-    assert!(!eval);
-    assert_eq!(counter.get(), 2);
-    todo!()
+    assert_eq!(none_of!({5,7,8,9}.map(|x|2*x)<6), true);
+    assert_eq!(none_of!({7,8,6,2,7}.satisfy(|x|x<6)), false);
 }
 
 #[test]
@@ -97,7 +59,7 @@ fn expressions_are_short_circuited_and_evaluated_left_to_right() {
 fn test_random_collection_of_values_behave_correctly() {
     let mut rng = thread_rng();
 
-    for _ in 1..1000000 {
+    for _ in 1..100000 {
         let a = rng.gen_range(-5..5);
         let b = rng.gen_range(-5..5);
         let c = rng.gen_range(-5..5);
@@ -105,30 +67,34 @@ fn test_random_collection_of_values_behave_correctly() {
         let rhs = rng.gen_range(-5..5);
 
         assert_all_eq!(
-            none_of!({a,b,c,d}.map(|x|x+1)==rhs),
-            none_of!({a,b,c,d}.satisfy(|x|x+1==rhs)),
-            [a, b, c, d].iter().map(|x|x+1).all(|v| !(v == rhs))
+            none_of!({a,b,c,d}.map(|x|x+1)==rhs+1),
+            none_of!({a,b,c,d}.satisfy(|x|x+1==rhs+1)),
+            [a, b, c, d].iter().map(|x|x+1).all(|v| !(v == rhs+1))
         );
-        todo!()
-        assert_eq!(
-            none_of!({a,b,c,d}<=rhs),
-            [a, b, c, d].iter().all(|v| !(v <= &rhs))
+        assert_all_eq!(
+            none_of!({a,b,c,d}.map(|x|2*x)<=2*rhs),
+            none_of!({a,b,c,d}.satisfy(|x|2*x <= 2*rhs)),
+            [a, b, c, d].iter().map(|x|2*x).all(|v| !(v <= 2*rhs))
         );
-        assert_eq!(
-            none_of!({a,b,c,d}>=rhs),
-            [a, b, c, d].iter().all(|v| !(v >= &rhs))
+        assert_all_eq!(
+            none_of!({a,b,c,d}.map(|x|x+1)>=rhs+1),
+            none_of!({a,b,c,d}.satisfy(|x|x+1>=rhs+1)),
+            [a, b, c, d].iter().map(|x|x+1).all(|v| !(v >= rhs+1))
         );
-        assert_eq!(
-            none_of!({a,b,c,d}>rhs),
-            [a, b, c, d].iter().all(|v| !(v > &rhs))
+        assert_all_eq!(
+            none_of!({a,b,c,d}.map(|x|333*x)>333*rhs),
+            none_of!({a,b,c,d}.satisfy(|x|333*x>333*rhs)),
+            [a, b, c, d].iter().map(|x|333*x).all(|v| !(v > rhs*333))
         );
-        assert_eq!(
-            none_of!({a,b,c,d}<rhs),
-            [a, b, c, d].iter().all(|v| !(v < &rhs))
+        assert_all_eq!(
+            none_of!({a,b,c,d}.map(|x|x-2)<rhs-2),
+            none_of!({a,b,c,d}.satisfy(|x|x-2<rhs-2)),
+            [a, b, c, d].iter().map(|x|x-2).all(|v| !(v < rhs-2))
         );
-        assert_eq!(
-            none_of!({a,b,c,d}!=rhs),
-            [a, b, c, d].iter().all(|v| !(v != &rhs))
+        assert_all_eq!(
+            none_of!({a,b,c,d}.map(|x|x+1)!=rhs),
+            none_of!({a,b,c,d}.satisfy(|x|x+1!=rhs)),
+            [a, b, c, d].iter().map(|x|x+1).all(|v| !(v != rhs))
         );
     }
 }
