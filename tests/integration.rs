2e55af58368ec5420c2fb9b92999fb1ce1d679b3
geo-ant <54497890+geo-ant@users.noreply.github.com>
2021-04-26 20:36:18 +0200

    add integration tests for chaining and nesting

--- tests/integration.rs
+++ tests/integration.rs
@@ -8,11 +8,20 @@ use fluent_comparisons::any_of;
 use fluent_comparisons::none_of;
 
 #[test]
+// test chaining the expressions
 fn test_chaining() {
-    assert!(all_of!({1,2,3} <= 4) && none_of!({-1,0,-100}>0) && any_of!({-1,2,3}==2));
+    assert!(all_of!({1,2,3} <= 4) && none_of!({-1,0,-100}>0) && any_of!({-1,2,3}==2)
+        && all_of!({2,4,6}.satisfy(|x|x%2==0)) && none_of!({2,3,4}.map(|x|x-1)>=4) && any_of!({1,2,3}.map(|x|x-2)<0));
 }
 
 #[test]
 fn test_nesting() {
-    todo!("add test for nesting all_of, none_of and any_of");
+    assert!(all_of!( {
+        all_of!({1,2,3} <= 4),
+        none_of!({-1,0,-100}>0),
+        any_of!({-1,2,3}==2),
+        all_of!({2,4,6}.satisfy(|x|x%2==0)),
+        none_of!({2,3,4}.map(|x|x-1)>=4),
+        any_of!({1,2,3}.map(|x|x-2)<0)
+    }.map(|b: bool| !b) == false));
 }
